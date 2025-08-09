41656424298cce358b7f33b849f402aea98062d7
Rishabh Rajhans <rrishu8877@icloud.com>
2025-08-09 21:36:40 +0530

    Your tool

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -57,6 +57,15 @@ export default function Footer() {
           </div>
         </div>
       </div>
+      {/* Google AdSense - unobtrusive, responsive ad */}
+      <div className="flex justify-center py-4">
+        <ins className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-xxxxxxxxxxxxxxxx"
+          data-ad-slot="1234567890"
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+      </div>
     </footer>
   );
 }
